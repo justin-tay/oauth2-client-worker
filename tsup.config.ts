73c8eb927bf46d132d1840e508b55ee75ec5c7df
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2023-10-14 11:07:51 +0800

    build: drop console for production builds

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,19 @@
+/* eslint-disable no-param-reassign */
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  target: 'es2020',
-  format: ['cjs', 'esm'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  dts: true,
+export default defineConfig((options) => {
+  const production = options.env?.NODE_ENV === 'production';
+  return {
+    target: 'es2020',
+    format: ['cjs', 'esm'],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    esbuildOptions(buildOptions) {
+      if (production) {
+        buildOptions.drop = ['console', 'debugger'];
+      }
+    },
+  };
 });
